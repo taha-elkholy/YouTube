617ef843834a03175530a44fc85bf85d9575f6b2
taha-elkholy <tahaelkholy.dev@gmail.com>
2020-03-27 18:31:24 +0200

    fix bugs in call response

--- app/src/main/java/com/troid/youtube/ui/videos/VideosListFragment.kt
+++ app/src/main/java/com/troid/youtube/ui/videos/VideosListFragment.kt
@@ -1,12 +1,10 @@
 package com.troid.youtube.ui.videos
 
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
@@ -15,7 +13,7 @@ import androidx.navigation.Navigation
 import com.troid.youtube.R
 import com.troid.youtube.adapter.VideosAdapter
 import com.troid.youtube.pojo.response.ItemsItem
-import kotlinx.android.synthetic.main.list_item.*
+import kotlinx.android.synthetic.main.fragment_videos_list.*
 
 class VideosListFragment : Fragment() {
 
@@ -32,7 +30,7 @@ class VideosListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        return inflater.inflate(R.layout.list_item, container, false)
+        return inflater.inflate(R.layout.fragment_videos_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
